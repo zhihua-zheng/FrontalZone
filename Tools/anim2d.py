ebdfbbea4151fb5de05827d60ccfb11a9ddca894
zhihua-zheng <greengo@outlook.com>
2023-11-05 21:09:58 -0500

    Add tool to merge multiple model ouputs

--- Tools/anim2d.py
+++ Tools/anim2d.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
 import warnings
 import argparse
-import sys
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,7 +14,7 @@ from viztool import FormatScalarFormatter
 def main():
     # process input arguments
     parser = argparse.ArgumentParser(description="""
-        Animate top slice and cross front transect from Oceananigans simualtion. Accept multiple variables.""")
+            Animate top slice and cross front transect from Oceananigans simualtion. Accept multiple variables.""")
     parser.add_argument('-f', '--file', action='store', dest='fname',
             metavar='FILENAME', help='Input simulation data')
     #parser.add_argument('-f2', '--file2', action='store', dest='fname2',
@@ -39,46 +39,50 @@ def main():
         parser.print_help()
         sys.exit(1)
     
-    data_dir = '/glade/work/zhihuaz/Data/FrontalZone/'
-    figs_dir = '/glade/u/home/zhihuaz/Projects/TRACE-SEAS/FrontalZone/Figures/'
+    # specify file path
+    if sys.platform == 'linux' or sys.platform == 'linux2':
+        data_dir = '/glade/work/zhihuaz/Data/FrontalZone/'
+        figs_dir = '/glade/u/home/zhihuaz/Projects/TRACE-SEAS/FrontalZone/Figures/'
+    elif sys.platform == 'darwin':
+        data_dir = '/Users/zhihua/Documents/Work/Research/Projects/TRACE-SEAS/FrontalZone/Data/'
+        figs_dir = '/Users/zhihua/Documents/Work/Research/Projects/TRACE-SEAS/FrontalZone/Figures/'
+    else:
+        print('OS not supported.')
     
     # read data
-    ds = xr.open_dataset(data_dir+args.fname).load()
+    ds = xr.open_dataset(data_dir+args.fname).load()#.chunk('auto')
     ds.close()
     
-    # add background fields
-    H = abs(ds.zF[0])
-    ds['Bt'] = (-ds.M2 * ds.xC + ds.B).transpose('time','zC','xC')
-    ds['Vt'] = (-ds.M2/ds.f * (ds.zC + H) + ds.V).transpose('time','zC','xC')
+    # time interval and initial timestamp in unit of hours
+    dhr = int(((ds.time[2] - ds.time[1])/np.timedelta64(1,'h')).data)
+    hr0 = int((ds.time[0]/np.timedelta64(1,'h')).data)
     
-    # time interval in unit of hours
-    ds = int(((ds.time[1] - ds.time[0])/np.timedelta64(1,'h')).data)
-    
-    fig = plt.figure(figsize=(4.8,6), constrained_layout=True)
-    gs = gridspec.GridSpec(8,1, figure=fig, left=0.1, right=0.9, bottom=0.1, top=0.9,
+    fig = plt.figure(figsize=(5,6), constrained_layout=True)
+    gs = gridspec.GridSpec(9,1, figure=fig, left=0.1, right=0.9, bottom=0.1, top=0.9,
                            wspace=0.05, hspace=0.05)
-
+    itime = 0
+    
     ax1 = fig.add_subplot(gs[:6, 0])
     kw_w1 = {'norm': mcolors.CenteredNorm(),
-           'levels': np.linspace(-3e-3,3e-3,31),
+           'levels': np.linspace(-9e-3,9e-3,31),
            'extend': 'both',
            'cmap': 'RdBu_r'
           }
-    kw_b1 = {'levels': np.arange(12,17,0.5)*1e-5,
+    kw_b1 = {'levels': np.arange(12,18,0.5)*1e-5,
            'colors': 'k',
-           'linewidths': 1
+           'linewidths': 0.2
           }
     Cw1 = ax1.contourf(ds.xC, ds.yC, ds.w.isel(time=itime), **kw_w1)
     Cb1 = ax1.contour(ds.xC, ds.yC, ds.bt.isel(time=itime), **kw_b1)
     cbar1 = fig.colorbar(Cw1, ax=ax1, fraction=0.027, format=FormatScalarFormatter('%.0f'))
-
+    
     ax2 = fig.add_subplot(gs[6:, 0], sharex=ax1)
     kw_w2 = {'norm': mcolors.CenteredNorm(),
-           'levels': np.linspace(-6e-4,6e-4,31),
+           'levels': np.linspace(-9e-4,9e-4,31),
            'extend': 'both',
            'cmap': 'RdBu_r'
           }
-    kw_b2 = {'levels': np.arange(-1,17,0.5)*1e-5, #np.arange(-1,2.36,0.07)*1e-5, 
+    kw_b2 = {'levels': np.arange(-1,18,0.5)*1e-5,
            'colors': 'k', 
            'linewidths': 1
           }
@@ -86,34 +90,29 @@ def main():
     Cb2 = ax2.contour(ds.xC, ds.zC, ds.Bt.isel(time=itime), **kw_b2)
     cbar2 = fig.colorbar(Cw2, ax=ax2, fraction=0.5, format=FormatScalarFormatter('%.0f'))
     
-    ani = animation.FuncAnimation(fig=fig, func=update, init_func=init, frames=np.arange(1, ds.dims['time']),
-                                  interval=400, repeat_delay=800, blit=True)
+    # def init():
+    cbar1.set_label(r'w|$_{z \approx -10 \:m}$ [m $s^{-1}$]', labelpad=-50)
+    cbar1.formatter.set_powerlimits((0, 0))
+    cbar1.formatter.set_useMathText(True)
+    cbar1.ax.yaxis.set_offset_position('left')
+    cbar1.ax.set_yticks([-9e-3, -6e-3, -3e-3, 0, 3e-3, 6e-3, 9e-3])
+    ax1.set_title(f't = {(itime*dhr)//24} days, {(itime*dhr)%24} hours')
+    ax1.set(aspect=1)
+    ax1.axes.get_xaxis().set_visible(False)
+    ax1.set_ylabel('Y [m]')
     
-    # save animation
-    ani.save(figs_dir+args.fname_out, writer='ffmpeg', fps=3, dpi=200)
-
-    def init():
-        cbar1.set_label(r'w|$_{z=-10 \:m}$ [m $s^{-1}$]', labelpad=-50)
-        cbar1.formatter.set_powerlimits((0, 0))
-        cbar1.formatter.set_useMathText(True)
-        cbar1.ax.yaxis.set_offset_position('left')
-        cbar1.ax.set_yticks([-3e-3, -2e-3, -1e-3, 0, 1e-3, 2e-3, 3e-3])
-        ax1.set_title(f't = {(itime*dhr)//24} days, {(itime*dhr)%24} hours')
-        ax1.set(aspect=1)
-        ax1.set_ylabel('Y [m]')
-
-        cbar2.set_label(r'$\langle w\rangle_{y}$ [m $s^{-1}$]', labelpad=-50)
-        cbar2.formatter.set_powerlimits((0, 0))
-        cbar2.formatter.set_useMathText(True)
-        cbar2.ax.yaxis.set_offset_position('left')
-        cbar2.ax.set_yticks([-6e-4, -4e-4, -2e-4, 0, 2e-4, 4e-4, 6e-4])
-        ax2.set_ylim(-100,0)
-        ax2.set_ylabel('Z [m]')
-        ax2.set_xlabel('X [m]')
-        return Cw1, Cb1, Cw2, Cb2
-
+    cbar2.set_label(r'$\langle w\rangle_{y}$ [m $s^{-1}$]', labelpad=-50)
+    cbar2.formatter.set_powerlimits((0, 0))
+    cbar2.formatter.set_useMathText(True)
+    cbar2.ax.yaxis.set_offset_position('left')
+    cbar2.ax.set_yticks([-9e-4, -6e-4, -3e-4, 0, 3e-4, 6e-4, 9e-4])
+    ax2.set_ylim(-100,0)
+    ax2.set_ylabel('Z [m]')
+    ax2.set_xlabel('X [m]')
+        # return Cw1, Cb1, Cw2, Cb2
+    
     def update(frame):
-        global Cw1, Cb1, Cw2, Cb2
+        nonlocal Cw1, Cb1, Cw2, Cb2
         # for each frame, get new data and clear old data stored on each artist
         wtop = ds.w.isel(time=frame)
         btop = ds.bt.isel(time=frame)
@@ -129,8 +128,14 @@ def main():
         Cb1 = ax1.contour(ds.xC, ds.yC, btop, **kw_b1)
         Cw2 = ax2.contourf(ds.xC, ds.zF, w_ymean, **kw_w2)
         Cb2 = ax2.contour(ds.xC, ds.zC, b_ymean, **kw_b2)
-        ax1.set_title(f't = {(frame*dhr)//24} days, {(frame*dhr)%24} hours');
+        ax1.set_title(f't = {(frame*dhr + hr0)//24} days, {(frame*dhr + hr0)%24} hours')
         return Cw1, Cb1, Cw2, Cb2
+    
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=range(ds.dims['time']), #init_func=init, 
+                                  interval=400, repeat_delay=800)#, blit=True
+    # save animation
+    ani.save(figs_dir+args.fname_out, writer='ffmpeg', fps=3, dpi=200)
+
 
 if __name__ == "__main__":
     main()
--- Tools/merge_outputs.py
+++ Tools/merge_outputs.py
@@ -1,10 +1,61 @@
 #!/usr/bin/env python3
 
+import sys
+import argparse
 import xarray as xr
 import numpy as np
+from dask.diagnostics import ProgressBar
 
 def main():
+    # process input arguments
+    parser = argparse.ArgumentParser(description="""
+            Merge several outputs from Oceananigans simualtion into one data file.""")
+    parser.add_argument('-c', '--case', action='store', dest='cname',
+            metavar='CASENAME', help='Simulation case name')
+    # parser.add_argument('-o', '--output', action='store', dest='fname_out',
+    #         metavar='FIGNAME', help='Output figure name')
+    parser.add_argument('--version', action='version', version='%(prog)s: 1.0')
+    # parsing arguments and save to args
+    args = parser.parse_args()
 
+    # check input
+    if not args.cname:
+        print('Oceananigans simulation case name are required. Stop.\n')
+        parser.print_help()
+        sys.exit(1)
     
+    # specify file path
+    if sys.platform == 'linux' or sys.platform == 'linux2':
+        data_dir = '/glade/work/zhihuaz/Data/FrontalZone/'
+    elif sys.platform == 'darwin':
+        data_dir = '/Users/zhihua/Documents/Work/Research/Projects/TRACE-SEAS/FrontalZone/Data/'
+    else:
+        print('OS not supported.')
+
+    # read data
+    with xr.open_dataset(data_dir+args.cname+'_top_slice.nc').chunk('auto').isel(zC=0,zF=0) as ds_top:
+        with xr.open_dataset(data_dir+args.cname+'_averages.nc').chunk('auto') as ds_average:
+             ds = xr.merge([ds_average, ds_top])
+    # ds_east = xr.open_dataset(data_dir+args.cname+'_east_slice.nc').load()
+    # ds_east.close()
+    # ds_south = xr.open_dataset(data_dir+args.cname+'_south_slice.nc').load()
+    # ds_south.close()
+
+    # # make the restart time as day 0
+    # if args.cname == 'spinup':
+    #     ds = ds.assign_coords(time=(ds.time - np.timedelta64(24*3+12,'h')))
+    
+    # add background fields
+    H = abs(ds.zF[0])
+    ds['bt'] = (-ds.attrs['M²'] * ds.xC + ds.b).transpose('time','yC','xC')
+    ds['vt'] = (-ds.attrs['M²']/ds.f * (ds.zC.isel(zC=-6).data + H) + ds.v).transpose('time','yF','xC')
+    ds['Bt'] = (-ds.attrs['M²'] * ds.xC + ds.B).transpose('time','zC','xC')
+    ds['Vt'] = (-ds.attrs['M²']/ds.f * (ds.zC + H) + ds.V).transpose('time','zC','xC')
+    
+    delayed_obj = ds.to_netcdf(data_dir+args.cname+'.nc', compute=False)
+    with ProgressBar():
+        results = delayed_obj.compute()
+
+
 if __name__ == "__main__":
-main()
+    main()
