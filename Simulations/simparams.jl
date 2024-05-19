81a35337aac34c8b4fe3c8647a151fd2a38df6eb
Zhihua Zheng <greengo@outlook.com>
2024-05-19 11:50:41 -0600

    Add N² flag for no front cases

--- Simulations/simparams.jl
+++ Simulations/simparams.jl
@@ -18,9 +18,8 @@ using Parameters
                out_interval_slice = 15minutes, # how often to save slice
                out_interval_mean  = 0.5minutes, # how often to save mean
 
-               z_refinement = 3,  # controls spacing near surface (higher means finer spaced)
-               z_stretching = 5,  # controls rate of stretching at bottom
-               sponge_σ     = 6,    # [m] sponge layer Gaussian mask width
+               z_refinement = 3.7,  # controls spacing near surface (higher means finer spaced)
+               z_stretching = 4,  # controls rate of stretching at bottom
                damping_rate = 1/60, # [s⁻¹] relax fields on a time-scale comparable to N₁, following Taylor & Ferrari 2010
  
                # whether to apply additional forcing to cancel the geostrophic stress associated with the background velocity
@@ -60,26 +59,28 @@ using Parameters
                )
 
     NoFront = (; commons...,
-               Lx = 1kilometers,
-               Ly = 1kilometers,
-               Nx_full = 512,
-               Ny_full = 512,
-               Nz_full = 64,
-               nTf = 9,
+               Lx = 250meters,
+               Ly = 250meters,
+               Nx_full = 128,
+               Ny_full = 128,
+               Nz_full = 128,
+               nTf = 4,
                cfl = 0.65,
-               max_Δt = 2minutes,
+               max_Δt = 0.5minutes,
                start_from_restratified = false,
-               pickup_checkpoint = true,
+               pickup_checkpoint = false,
                ckpdir_affix = "Regular",
                )
 end
 
 
 function decode_casename(casename)
-    fres, ∂b_∂x, heat_flux, wind_stress, wind_direction, Stokes_flag = split(casename, "_")
-    coarsen_factor_h = parse(Int64, fres[2])
-    coarsen_factor_z = parse(Int64, fres[3])
-    M² = parse(Float64, lstrip(∂b_∂x,          'M')) / 1e8 # [s⁻²] horizontal buoyancy gradient
+    fres, b_gradient, heat_flux, wind_stress, wind_direction, Stokes_flag = split(casename, "_")
+    case_type = fres[1]
+    Gb_type   = ifelse(case_type=='n', 'N', 'M')
+    coarsen_h = parse(Int64, fres[2])
+    coarsen_z = parse(Int64, fres[3])
+    Gb = parse(Float64, lstrip(b_gradient, Gb_type)) / 1e8 # [s⁻²] horizontal or vertical buoyancy gradient
     Q₀ = parse(Float64, lstrip(heat_flux,      'Q'))
     τ₀ = parse(Float64, lstrip(wind_stress,    'W')) / 1e3
     if startswith(wind_direction, 'D')
@@ -92,30 +93,30 @@ function decode_casename(casename)
         throw(DomainError(wind_direction, "wind direction identifier not supoorted"))
     end
     use_Stokes = parse(Bool, Stokes_flag[end])
-    return coarsen_factor_h, coarsen_factor_z, M², Q₀, τ₀, θ₀, wind_oscillation, use_Stokes
+    return case_type, coarsen_h, coarsen_z, Gb, Q₀, τ₀, θ₀, wind_oscillation, use_Stokes
 end
 
 
 function enrich_parameters(params, casename)
-    coarsen_factor_h, coarsen_factor_z, M², Q₀, τ₀, θ₀, wind_oscillation, use_Stokes = decode_casename(casename)
-    #nTf        = ifelse(θ₀==90, 3, 4)
-    Lz         = ifelse(θ₀==90, 70meters, 140meters) # depth
+    case_type, coarsen_h, coarsen_z, Gb, Q₀, τ₀, θ₀, wind_oscillation, use_Stokes = decode_casename(casename)
+    nTf        = ifelse(θ₀==90, 3, 4)
+    Lz         = ifelse((θ₀==90) & (case_type=='s'), 84meters, 147meters) # depth
     sponge_σ   = round(√((Lz/7)^2 / 6 / 2), sigdigits=3) # [m] sponge layer Gaussian mask width (thickness: Lz/7, tapering to e⁻⁶)
     σ_wind     = ifelse(wind_oscillation, params.f, 0)
     wind_waves = ifelse((τ₀==0) & use_Stokes, false, true)
 
-    # vertical gradient of along-front velocity at the boundary
-    ∂v∂z_cgeo = ifelse(params.counter_geo_stress, M²/params.f, 0)
-    N₀² = ifelse(startswith(casename, 'n'), 9e-8, params.RiB₀*(M²)^2/(params.f)^2) # [s⁻²] mixed layer buoyancy frequency
-    N₁² = params.RiB₁*N₀² # [s⁻²] thermocline buoyancy frequency
+    M²  = ifelse(case_type=='n', 0,   Gb)
+    N₀² = ifelse(case_type=='n', Gb,  params.RiB₀*(M²)^2/(params.f)^2) # [s⁻²] mixed layer buoyancy frequency
+    N₁² = ifelse(case_type=='n', N₀², params.RiB₁*N₀²) # [s⁻²] thermocline buoyancy frequency
     Tf  = 2π/params.f   # [s] inertial period
     t₀  = ifelse(params.pickup_checkpoint, 6*Tf, 0) # [s] when to introduce wind stress
     tᵣ  = √2*Tf # [s] length of the linear ramp of wind forcing
     ckp_interval = 1*Tf # [s] how often to checkpoint
+    ∂v∂z_cgeo    = ifelse(params.counter_geo_stress, M²/params.f, 0) # vertical gradient of along-front velocity at top/bottom
 
-    Nx = params.Nx_full ÷ coarsen_factor_h
-    Ny = params.Ny_full ÷ coarsen_factor_h
-    Nz = params.Nz_full ÷ coarsen_factor_z
+    Nx = params.Nx_full ÷ coarsen_h
+    Ny = params.Ny_full ÷ coarsen_h
+    Nz = params.Nz_full ÷ coarsen_z
 
     τ₀ˣ   = τ₀ * cosd(θ₀)
     τ₀ʸ   = τ₀ * sind(θ₀)
@@ -131,6 +132,7 @@ function enrich_parameters(params, casename)
 
     extra_params = Base.@locals()
     delete!(extra_params, :params)
+    delete!(extra_params, :Gb)
     delete!(extra_params, :casename)
     return merge(params, NamedTuple(extra_params))
 end
